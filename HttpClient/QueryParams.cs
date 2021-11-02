9e4809d0c231a83c7a1efaa9bc1fb2656226c98b
emresolak <s.emresolak@dreamharvesters.com>
2021-11-02 17:32:22 +0300

    introduced QueryParams, removed Document model, BucketServer.Data.GetAll

---
+++ HttpClient/QueryParams.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpicaSDK
+{
+    public class QueryParams
+    {
+        public Dictionary<string, string> Value { get; private set; }
+
+        public QueryParams() : this(16)
+        {
+        }
+
+        public QueryParams(int count)
+        {
+            Value = new Dictionary<string, string>(count);
+        }
+
+        public string QueryString
+        {
+            get
+            {
+                StringBuilder queryBuilder = new StringBuilder();
+                foreach (var queryStr in Value)
+                {
+                    queryBuilder.Append($"{queryStr.Key}:{queryStr.Value}");
+                }
+
+                return queryBuilder.ToString();
+            }
+        }
+    }
+}
