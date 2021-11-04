80a69252f375155cc0dda11ece7ee3391cd053b6
emresolak <s.emresolak@dreamharvesters.com>
2021-11-04 13:29:59 +0300

    realtime

--- HttpClient/QueryParams.cs
+++ HttpClient/QueryParams.cs
@@ -6,7 +6,7 @@ namespace SpicaSDK
     public class QueryParams
     {
         public static string Empty => string.Empty;
-        public Dictionary<string, string> Value { get; private set; }
+        private Dictionary<string, string> value;
 
         public QueryParams() : this(16)
         {
@@ -14,15 +14,17 @@ namespace SpicaSDK
 
         public QueryParams(int count)
         {
-            Value = new Dictionary<string, string>(count);
+            value = new Dictionary<string, string>(count);
         }
+
+        public void AddQuery(string key, string value) => this.value.Add(key, value);
 
         public string QueryString
         {
             get
             {
                 StringBuilder queryBuilder = new StringBuilder();
-                foreach (var queryStr in Value)
+                foreach (var queryStr in value)
                 {
                     queryBuilder.Append($"{queryStr.Key}:{queryStr.Value}");
                 }
